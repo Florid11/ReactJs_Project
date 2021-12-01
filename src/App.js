35611665c2412e8419acc97618abf4ec7da75343
Florid11 <florid.arifaj@hotmail.com>
2021-12-01 19:51:46 +0100

    Add Costumer Component was added.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CustomersComponent from "./components/Customers.component";
 import HeaderComponent from "./components/Header.component";
 import FooterComponent from "./components/Footer.component";
 import productComponent from "./components/product.components";
+import AddCustomerComponents from "./components/AddCustomer.components";
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
                       <Route exact path="/" component={CustomersComponent}/>
                       <Route exact path="/customers" component={CustomersComponent}/>
                       <Route exact path="/products" component={productComponent}/>
+                      <Route exact path="/add-customer" component={AddCustomerComponents}/>
+
                   </Switch>
                 </div>
                 <FooterComponent/>

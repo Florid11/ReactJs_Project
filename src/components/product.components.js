1339335c2ca47c5b9651ecdaea62561d4ff1239c
Florid11 <florid.arifaj@hotmail.com>
2021-12-03 17:47:33 +0100

    Started working at components

--- src/components/product.components.js
+++ src/components/product.components.js
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import ProductDataService from "../services/Products.services";
 
 class productComponent extends Component{
     constructor(props) {
@@ -10,6 +11,9 @@ class productComponent extends Component{
     }
 
     componentDidMount() {
+        ProductDataService.getProducts().then((res)=>{
+           this.setState({products:res.data});
+        });
     }
 
 

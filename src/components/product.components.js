351caa42d81f1a7376ef0427f662d710f7eca7ac
Florid11 <florid.arifaj@hotmail.com>
2021-12-03 18:31:45 +0100

    Products Listed

--- src/components/product.components.js
+++ src/components/product.components.js
@@ -21,28 +21,30 @@ class productComponent extends Component{
         return(
             <div>
                 <h2 className="text-center">Products List</h2>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                        <tr>
-                            <th>Indetification</th>
-                            <th>Label</th>
-                            <th>Description</th>
-                            <th>Price</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            <tr>
-                                <td>12345</td>
-                                <td>Emri i Produktit</td>
-                                <td>Pershkrimi i Produktit</td>
-                                <td>249.99 EUR</td>
-                            </tr>
-                        }
-                        </tbody>
-                    </table>
-                </div>
+                    <div className="row">
+                        <table className="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Identification</th>
+                                    <th>Label</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.products.map(product =>
+                                        <tr key={product.id}>
+                                            <td>{product.id}</td>
+                                            <td>{product.label}</td>
+                                            <td>{product.description}</td>
+                                            <td>{product.price}</td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
             </div>
         )
     }
